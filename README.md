11d71949f91175021d55f1a7ce6ef4c704ec1e2c
Lexie <6937574+Lexiebean@users.noreply.github.com>
2023-04-10 13:34:54 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# pfUI PvP Overlay
+
+An extension for pfUI which adds faction coloured dragon textures to unitframes frames.
+
+## Credits
+Artwork by [GaMu-ChAn](https://www.deviantart.com/gamu-chan/art/WOW-Elite-frame-143568653)
+Shagu for pfUI-Elite Overlay
---
+++ pfUI-pvpoverlay.lua
@@ -0,0 +1,89 @@
+pfUI:RegisterModule("PvPOverlay", "vanilla:tbc", function ()
+  pfUI.gui.dropdowns.PvPOverlay_positions = {
+    "left:" .. T["Left"],
+    "right:" .. T["Right"],
+    "off:" .. T["Disabled"]
+  }
+
+  -- detect current addon path
+  local addonpath
+  local tocs = { "", "-master", "-tbc", "-wotlk" }
+  for _, name in pairs(tocs) do
+    local current = string.format("pfUI-pvpoverlay%s", name)
+    local _, title = GetAddOnInfo(current)
+    if title then
+      addonpath = "Interface\\AddOns\\" .. current
+      break
+    end
+  end
+
+  if pfUI.gui.CreateGUIEntry then -- new pfUI
+    pfUI.gui.CreateGUIEntry(T["Thirdparty"], T["PvP Overlay"], function()
+      pfUI.gui.CreateConfig(pfUI.gui.UpdaterFunctions["target"], T["Select dragon position"], C.PvPOverlay, "position", "dropdown", pfUI.gui.dropdowns.PvPOverlay_positions)
+    end)
+  else -- old pfUI
+    pfUI.gui.tabs.thirdparty.tabs.PvPOverlay = pfUI.gui.tabs.thirdparty.tabs:CreateTabChild("PvPOverlay", true)
+    pfUI.gui.tabs.thirdparty.tabs.PvPOverlay:SetScript("OnShow", function()
+      if not this.setup then
+        local CreateConfig = pfUI.gui.CreateConfig
+        local update = pfUI.gui.update
+        this.setup = true
+      end
+    end)
+  end
+
+  pfUI:UpdateConfig("PvPOverlay",       nil,         "position",   "right")
+
+
+  local HookRefreshUnit = pfUI.uf.RefreshUnit
+  function pfUI.uf:RefreshUnit(unit, component)
+    local pos = string.upper(C.PvPOverlay.position)
+    local invert = C.PvPOverlay.position == "right" and 1 or -1
+    local unitstr = ( unit.label or "" ) .. ( unit.id or "" )
+
+    local size = unit:GetWidth() / 1.5
+    local pvp = UnitIsPVP(unitstr)
+	local faction = UnitFactionGroup(unitstr)
+
+    unit.dragonTop = unit.dragonTop or unit:CreateTexture(nil, "OVERLAY")
+    unit.dragonBottom = unit.dragonBottom or unit:CreateTexture(nil, "OVERLAY")
+
+    if unitstr == "" or C.PvPOverlay.position == "off" then
+      unit.dragonTop:Hide()
+      unit.dragonBottom:Hide()
+    else
+      unit.dragonTop:ClearAllPoints()
+      unit.dragonTop:SetWidth(size)
+      unit.dragonTop:SetHeight(size)
+      unit.dragonTop:SetPoint("TOP"..pos, unit, "TOP"..pos, invert*size/5, size/7)
+      unit.dragonTop:SetParent(unit.hp.bar)
+
+      unit.dragonBottom:ClearAllPoints()
+      unit.dragonBottom:SetWidth(size)
+      unit.dragonBottom:SetHeight(size)
+      unit.dragonBottom:SetPoint("BOTTOM"..pos, unit, "BOTTOM"..pos, invert*size/5.2, -size/2.98)
+      unit.dragonBottom:SetParent(unit.hp.bar)
+
+      if pvp == 1 and faction == "Horde" then
+        unit.dragonTop:SetTexture(addonpath.."\\img\\TOP_GRAY_"..pos)
+        unit.dragonTop:Show()
+        unit.dragonTop:SetVertexColor(.55,.09,.09,1)
+        unit.dragonBottom:SetTexture(addonpath.."\\img\\BOTTOM_GRAY_"..pos)
+        unit.dragonBottom:Show()
+        unit.dragonBottom:SetVertexColor(.55,.09,.09,1)
+      elseif pvp == 1 and faction == "Alliance" then
+        unit.dragonTop:SetTexture(addonpath.."\\img\\TOP_GRAY_"..pos)
+        unit.dragonTop:Show()
+        unit.dragonTop:SetVertexColor(.18,.29,.58,1)
+        unit.dragonBottom:SetTexture(addonpath.."\\img\\BOTTOM_GRAY_"..pos)
+        unit.dragonBottom:Show()
+        unit.dragonBottom:SetVertexColor(.18,.29,.58,1)
+      else
+        unit.dragonTop:Hide()
+        unit.dragonBottom:Hide()
+      end
+    end
+
+    HookRefreshUnit(this, unit, component)
+  end
+end)
